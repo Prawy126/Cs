79973c5dfd9e3c88dfcdadf37ffb7124dc509919
Prawy_126 <michal190713@gmail.com>
2023-12-04 23:13:31 +0100

    Update

---
+++ lab4/lab4/Osoba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class Osoba
+    {
+        string imie, nazwisko;
+        string  pesel;
+
+        public void SetName(string imie)
+        {
+            this.imie = imie;
+        }
+        public void SetLastName(string nazwisko)
+        {
+            this.nazwisko = nazwisko;
+        }
+        public void SetPesel(string pesel)
+        {
+            this.pesel = pesel;
+        }
+        public int GetAge()
+        {
+            int year = int.Parse(1900 + pesel.Substring(0, 2));
+            int currentYear = DateTime.Now.Year;
+            return currentYear - year;
+        }
+    }
+}
--- lab4/lab4/obj/Debug/net8.0/lab4.AssemblyInfo.cs
+++ lab4/lab4/obj/Debug/net8.0/lab4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed8fab2055505b4d8df172259f3e38af2e51a1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
