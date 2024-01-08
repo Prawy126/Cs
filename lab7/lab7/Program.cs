3fac98ba8441f8d3549182cb32a3e2910ecdc53f
Prawy_126 <michal190713@gmail.com>
2024-01-08 17:15:08 +0100

    Zajęcia 08.01.2024

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper;
+using lab7;
+using System.Globalization;
+
+
+string filePath = "sample.csv";
+
+WriteDataToCsv(filePath);
+
+SearchDataInCSV(filePath, "John");
+
+Console.WriteLine();
+
+static void WriteDataToCsv(string filePath)
+{
+    List<Person> people = new ;
+}
